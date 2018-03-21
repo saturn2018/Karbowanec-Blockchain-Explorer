3e3304250c32d56917bf30ea7f16c133488fd429
saturn2018 <37569744+saturn2018@users.noreply.github.com>
2018-03-21 13:56:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-var api = 'http://104.211.244.63:8117';
-var blockTargetInterval = 240;
-var coinUnits = 1000000000000;
+var api = 'http://127.0.0.1:8117';
+var blockTargetInterval = 30;
+var coinUnits = 100;
 var symbol = 'TAN';
 var refreshDelay = 30000;
 // pools stats by MainCoins
